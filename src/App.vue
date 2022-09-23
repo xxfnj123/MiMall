13bbeec253c71dfd6aab8d833725b833ac3a0f1c
xxfnj123 <2623734202@qq.com>
2022-09-23 09:13:36 +0800

    跨越

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,35 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+//import axios from 'axios'
+import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      age:30,
+      data:''
+    }
+  },
+  mounted () {
+
+    //axios -- 会发送一个XHR请求
+    //let url = "https://mock.presstime.cn/mock/632b13eab98faa00824e4ee9/example/restful/:id/list";
+    /* 
+    axios.get(url).then(()=>{
+
+    }) 
+    */
+
+    //jsonp -- 只是一段js脚本，没有发送请求
+    let url = "https://www.imooc.com/activity/servicetime";
+    jsonp(url,(err,res)=>{
+      let result = res;
+      this.data = result;
+    })
   }
 }
 </script>
