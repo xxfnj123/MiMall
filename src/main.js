55d1f46365e9e5533356ae4f0e8d50b0757a1817
xxfnj123 <2623734202@qq.com>
2022-09-29 09:15:33 +0800

    mock

--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
-import env from './env'
-
+//import env from './env'
+//mock开关
+const mock = false;
+if(mock){
+  require('./mock/api');
+}
 //根据前端的跨越方式做调整  /a/b : /api/a/b => /a/b
-axios.defaults.baseURL = '/api';
+axios.defaults.baseURL = 'https://mock.presstime.cn/mock/6334ef7703bda8005d332324/mockapi';
+//axios.defaults.baseURL = '/api';
 axios.defaults.timeout = 8000;
 //根据环境变量获取不同的请求地址
-axios.defaults.baseURL = env.baseURL;
+//axios.defaults.baseURL = env.baseURL;
 
 //接口错误拦截
 axios.interceptors.response.use(function(response){
