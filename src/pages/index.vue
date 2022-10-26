6abe12c2f7caf721ec2a5f4de5635a108a49ac73
xxfnj123 <2623734202@qq.com>
2022-10-26 15:17:38 +0800

    ads

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -57,8 +57,16 @@
                     <div class="swiper-button-next" slot="button-next"></div>
                 </swiper>
             </div>
-            <div class="ads-box"></div>
-            <div class="banner"></div>
+            <div class="ads-box">
+                <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+                    <img :src="item.img" alt="">
+                </a>
+            </div>
+            <div class="banner">
+                <a href="/#/product/30">
+                    <img src="/imgs/banner-1.png" alt="">
+                </a>
+            </div>
             <div class="product-box"></div>
         </div>
         <service-bar></service-bar>
@@ -141,6 +149,24 @@
                         }
                     ],
                     [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+                ],
+                adsList:[
+                    {
+                        id:33,
+                        img:'/imgs/ads/ads-1.png'
+                    },
+                    {
+                        id:48,
+                        img:'/imgs/ads/ads-2.jpg'
+                    },
+                    {
+                        id:48,
+                        img:'/imgs/ads/ads-3.png'
+                    },
+                    {
+                        id:47,
+                        img:'/imgs/ads/ads-4.jpg'
+                    }
                 ]
             }
         }
@@ -227,6 +253,18 @@
                     height: 100%;
                 }
             }
+        }
+        .ads-box{
+            @include flex();
+            margin-top: 14px;
+            margin-bottom: 31px;
+            a{
+                width: 296px;
+                height: 167px;
+            }
+        }
+        .banner{
+            margin-bottom: 50px;
         }
     }
 </style>
